eca66fec4ded429906b5d220d4d3c5f229503cee
Thomas Perale <thomas.perale@openmailbox.org>
2018-11-12 19:24:35 +0100

    [add] unstated package to track the stats

---
+++ containers/Stats.js
@@ -0,0 +1,11 @@
+import React import 'react';
+import { Provider, Subscribe, Container } from 'unstated';
+
+class CounterContainer extends Container {
+  state = {
+    ulb: 0,
+    cercles: 0,
+    ace: 0,
+    sponsors: 0,
+  };
+}
