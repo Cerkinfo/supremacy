26710461da35da054efb9db064c805f96c0f4443
Thomas Perale <thomas.perale@openmailbox.org>
2018-11-12 20:47:12 +0100

    [fix] wrong name convention

---
+++ containers/StatsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'unstated';
+
+export default class _ extends Container {
+  state = {
+    ulb: 0,
+    cercles: 0,
+    ace: 0,
+    sponsors: 0,
+  };
+}
